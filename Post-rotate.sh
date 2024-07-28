d0f5574f960d09dcc293602219d0a60a790b5c37
rashid-007 <91994486+rashid-007@users.noreply.github.com>
2024-07-29 01:10:58 +0530

    Update Post-rotate.sh
    new changes

--- Post-rotate.sh
+++ Post-rotate.sh
@@ -3,7 +3,6 @@
     rotate 4
     compress
     postrotate
-        /usr/bin/systemctl reload myapp >/dev/null 2> /dev/null \
-|| true
+        /usr/bin/systemctl reload myapp >/dev/null 2> /dev/null || true
     endscript
 }
